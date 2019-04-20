90817911d2fcdc5997269fb339f6c4bf2a246fcf
Christian Pinto <pintochristian2@gmail.com>
2019-04-20 23:51:05 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,9 +15,11 @@ end
 
 def take_a_number(line_array, new_person)
   position = 1
+  if line_array.size == 0
   line = ""
   line_array.each do |string|
     line << ("Welcome, #{new_person}. You are number #{position} in line.")
     position+=1
   end
+end
 end
