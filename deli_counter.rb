20eaf89d47db9714ba37d012609b9bfed8c47052
Christian Pinto <pintochristian2@gmail.com>
2019-04-20 21:49:54 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,4 +1,10 @@
 # Write your code here.
-def line
-  
+def line(line)
+  if line.size == 0
+    puts "The line is currently empty."
+  else
+    return line
+  end
 end
+
+def take_a_number(line, new_person)
