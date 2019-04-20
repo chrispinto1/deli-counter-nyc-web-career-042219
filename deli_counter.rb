90ecf7dbc9e84cf9cf39c189f2b1f78611593936
Christian Pinto <pintochristian2@gmail.com>
2019-04-20 21:55:59 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,9 @@ def line(line)
   if line.size == 0
     puts "The line is currently empty."
   else
-    return line
+    i = 1
+    line.each do |name| 
+      puts "The line is currently"
   end
 end
 
